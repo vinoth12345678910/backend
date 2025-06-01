a5028390cef487f7245bac9947c3d1238e8e323b
vinoth12345678910 <anandgani85@gmail.com>
2025-06-01 18:52:29 +0530

    Your commit message

---
+++ Promises/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    console.log(`Breed: ${data}`);
+    superagent.get(`https://dog.ceo/api/breeds/image/random/${data}`).end((err, res) => {
+
+        console.log(res.body);
+        fs.writeFile('dog-img.txt', res.body.message, err => {
+            if (err) return console.log(err);
+            console.log('Random dog image saved to file!');
+        });
+    
+})
+})
